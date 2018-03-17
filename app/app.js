62fd641a47c3575b84c76af66e5e2f5879874cd4
Pawel <pawel.klasa@gmail.com>
2018-03-17 08:06:45 +0000

    defined all vars

--- app/app.js
+++ app/app.js
@@ -1,1 +1,6 @@
 // Define UI vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
